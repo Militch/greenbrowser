f196a609295c3782593175d7267ee3eac07996f1
Militch <litch.me@gmail.com>
2021-10-25 10:36:33 +0800

    update

--- greenadmin-web/src/main/java/com/esiran/greenadmin/Application.java
+++ greenadmin-web/src/main/java/com/esiran/greenadmin/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 @MapperScan("com.esiran.greenadmin.**.mapper")
 public class Application {
     public static void main(String[] args) {
